1fb8424d1cf8172df8dac0e210297d6eec7b83f5
Stakhaevich <verast2006@gmail.com>
2026-03-30 02:10:16 +0300

    Тесты для класса MusicCollection

---
+++ MusicCollectionApp/MusicCollectionAppTests/MusicCollectionTests.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace MusicCollectionAppTests
+{
+    [TestClass]
+    public class MusicCollectionTests
+    {
+        [TestMethod]
+        public void TestMethod1()
+        {
+        }
+    }
+}
